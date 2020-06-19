2faba56128d437c01b50189e5043e247d0b68148
manuelschuppisser <60382887+manuelschuppisser@users.noreply.github.com>
2020-06-19 09:16:11 +0200

    Updates

---
+++ precache-manifest.bef00bde94a17d088ebcb90034dd477a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc3226739bf7f4bacdf40000a68d940",
+    "url": "/shop-redux-online/index.html"
+  },
+  {
+    "revision": "2781cc7ca40939f7f61e",
+    "url": "/shop-redux-online/static/css/main.03a7a7df.chunk.css"
+  },
+  {
+    "revision": "2d5a6e97fd387c9042f8",
+    "url": "/shop-redux-online/static/js/2.989c1754.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shop-redux-online/static/js/2.989c1754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2781cc7ca40939f7f61e",
+    "url": "/shop-redux-online/static/js/main.0030a342.chunk.js"
+  },
+  {
+    "revision": "84af8cfe42b0ff760c4f",
+    "url": "/shop-redux-online/static/js/runtime-main.9de58fbe.js"
+  },
+  {
+    "revision": "e0748f46858ca316e4d49f4255945ae6",
+    "url": "/shop-redux-online/static/media/VanGogh1886.e0748f46.jpg"
+  },
+  {
+    "revision": "e3550fc357a0049dc905b4895094ebd4",
+    "url": "/shop-redux-online/static/media/VanGogh1887.e3550fc3.jpg"
+  },
+  {
+    "revision": "1db24c0c2487fa45051e538cf6f2cc43",
+    "url": "/shop-redux-online/static/media/VanGogh1888.1db24c0c.jpg"
+  },
+  {
+    "revision": "68b510e08e79d422f34c03494137bb9a",
+    "url": "/shop-redux-online/static/media/VanGogh1889.68b510e0.jpg"
+  },
+  {
+    "revision": "b9c44b5a0cbd9621e6b912f6d031e5e8",
+    "url": "/shop-redux-online/static/media/header.b9c44b5a.jpg"
+  },
+  {
+    "revision": "e2556776103fb66f80737996079dc5d4",
+    "url": "/shop-redux-online/static/media/logo.e2556776.png"
+  }
+]);
